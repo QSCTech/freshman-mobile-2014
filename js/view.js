c14efd8dc566c7e9e4042837a69c4c03f04183cf
Senorsen <sen@senorsen.com>
2014-07-21 00:24:12 +0800

    offset

--- js/view.js
+++ js/view.js
@@ -105,7 +105,8 @@ Doc.prototype.initFunc = function() {
     };
     this.switchPage = function(page, gesture) {
         // gesture 用于判断是否为用户滑动。如果是，那么将采用其他动画。
-        $('#pages-container').animate({scrollTop: that.pages[page].offset().top});
+        var topOffset = 100;
+        $(document.body).scrollTop(topOffset + that.pages[page].offset().top);
     };
 };
 
