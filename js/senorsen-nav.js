6d5bf18d748dfe57206c9668120321d4ae34884a
Senorsen <sen@senorsen.com>
2014-07-22 02:10:11 +0800

    open || close your drawer freely

--- js/senorsen-nav.js
+++ js/senorsen-nav.js
@@ -9,32 +9,35 @@ var Nav = function() {
     this.status = false;
     this.tapevent = 'click';
     if (window.supportsTouch) this.tapevent = 'tap';
-    $('#nav-drawer-gray-layer')[this.tapevent](function() {
-        $(this).addClass('close');
+    
+    this.closeDrawer = function() {
+        $('#nav-drawer-gray-layer').addClass('close');
         $('#nav-drawer').addClass('close');
         $('#nav-bar-btn').removeClass('active');
         setTimeout(function() {
             $('#nav-drawer-gray-layer').addClass('realclose');
         }, 200);
         that.status = false;
-    });
+    };
+    this.openDrawer = function() {
+        $('#nav-drawer-gray-layer').removeClass('realclose').removeClass('close');
+        $('#nav-drawer').removeClass('close');
+        $('#nav-bar-btn').addClass('active');
+        that.status = true;
+    };
+    $('#nav-drawer-gray-layer')[this.tapevent](this.closeDrawer);
     $('#nav-bar-btn')[this.tapevent](function(e) {
         e.preventDefault();
         e.stopPropagation();
         if (that.status) {
             // 现在为打开状态，需要关闭
-            $(this).removeClass('active');
-            $('#nav-drawer-gray-layer').addClass('close');
-            $('#nav-drawer').addClass('close');
-            setTimeout(function() {
-                $('#nav-drawer-gray-layer').addClass('realclose');
-            }, 200);
+            that.closeDrawer();
         } else {
-            $(this).addClass('active');
-            $('#nav-drawer-gray-layer').removeClass('realclose').removeClass('close');
-            $('#nav-drawer').removeClass('close');
+            that.openDrawer();
         }
-        that.status = !that.status;
+    });
+    $('.drawer-btn').bind('click', function() {
+        that.closeDrawer();
     });
 };
 
